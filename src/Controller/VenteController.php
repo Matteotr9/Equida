284c78377767b8375e06f82530b5a9cd334f6cca
Matteotr9 <89576532+Matteotr9@users.noreply.github.com>
2023-02-16 11:43:18 +0100

    création liste des ventes

--- src/Controller/VenteController.php
+++ src/Controller/VenteController.php
@@ -5,15 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Vente;
+use Doctrine\Persistence\ManagerRegistry;
 
 class VenteController extends AbstractController
 {
-    #[Route('/vente', name: 'app_vente')]
-    public function index(): Response
+    #[Route('/vente/lister', name: 'app_vente_lister')]
+    public function getLesVentes(ManagerRegistry $doctrine): Response
     {
-        $ventes = $this->getDoctrine()
+        $entityManager = $doctrine->getManager();
+        $ventes = $entityManager->getRepository(Vente::class)->findAll();
+       /* $ventes = $this->getDoctrine()
             ->getRepository(Vente::class)
-            ->findAll();
+            ->findAll();*/
 
         return $this->render('vente/index.html.twig', [
             'ventes' => $ventes,
